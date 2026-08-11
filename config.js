fd096608554ae8432284367f4839413ad1945593
minhloc-2002 <minhloc20022013@gmail.com>
2026-08-11 10:19:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-const SITE_CONFIG={\n  repository:"seo.github.io",\n  baseUrl:"https://minhloc-2002.github.io/seo.github.io"\n};
+const SITE_CONFIG={
+  repository:"seo.github.io",
+  repoUrl:"https://github.com/minhloc-2002/seo.github.io",
+  baseUrl:"https://minhloc-2002.github.io/seo.github.io/"
+};
